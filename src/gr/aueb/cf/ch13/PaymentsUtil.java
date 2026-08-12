7540aae0265854fe8b715310c40e10904362701b
sotikour <sotikour@gmail.com>
2026-08-12 17:58:45 +0300

    chore(PaymentsUtil): Add Utility methods for rounding and calculating differences of BigDecimal.

---
+++ src/gr/aueb/cf/ch13/PaymentsUtil.java
@@ -0,0 +1,34 @@
+package gr.aueb.cf.ch13;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class PaymentsUtil {
+
+    private PaymentsUtil() {}
+
+    /**
+     * Rounds a BigDecimal to the specified number of decimal places
+     * @param value         the amount to round
+     * @param decimaPlaces  the number of Decimal places to round to.
+     * @return              the rounded amount.
+     */
+    public static BigDecimal round(BigDecimal value, int decimaPlaces) {
+        if (value == null) return BigDecimal.ZERO;
+        return value.setScale(decimaPlaces, RoundingMode.HALF_UP);
+
+    }
+
+    /**\
+     * Calculate the differences between two BigDecimals
+     * @param a                 the first BigDecimal
+     * @param b                 the second BigDecimal
+     * @param decimaPlaces      the number of Decimal places to round to.
+     * @return                  the difference between the two amounts.
+     */
+    public static BigDecimal diff(BigDecimal a, BigDecimal b, int decimaPlaces) {
+        if (a == null || b == null) return BigDecimal.ZERO;
+        return b.subtract(a)
+                .setScale(decimaPlaces, RoundingMode.HALF_UP);
+    }
+}
