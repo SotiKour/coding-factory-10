59639288490a7dc806d4a60a8816ae183388c245
sotikour <sotikour@gmail.com>
2026-08-25 19:12:06 +0300

    chore(Square): add Square class extending AbstractRectangle with constructors

---
+++ src/gr/aueb/cf/ch15/skeletal_impl/Square.java
@@ -0,0 +1,15 @@
+package gr.aueb.cf.ch15.skeletal_impl;
+
+import java.awt.*;
+
+public class Square extends AbstractRectangle {
+
+    public Square() {
+
+    }
+
+    public Square(double width, double height) {
+        super(width, height);
+    }
+
+}
