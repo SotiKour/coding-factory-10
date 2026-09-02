78cedd24c5593ab0dd386eb08930b4594d2cb018
sotikour <sotikour@gmail.com>
2026-09-02 19:18:18 +0300

    chore(Main): refactor Main class to include Point equality demonstration

--- src/gr/aueb/cf/ch16/copy_constructor/Main.java
+++ src/gr/aueb/cf/ch16/copy_constructor/Main.java
@@ -1,8 +1,8 @@
 package gr.aueb.cf.ch16.copy_constructor;
 
 public class Main {
-    City athens = new City("Athens");
-    City copyAthens = new City(athens);
-
-
+    public static void main(String[] args) {
+        City athens = new City("Athens");
+        City copyAthens = new City(athens);
+    }
 }
