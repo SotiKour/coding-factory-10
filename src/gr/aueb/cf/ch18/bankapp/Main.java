86b348cd1665d2ac0de4ff02317ac80a8e3de047
sotikour <sotikour@gmail.com>
2026-09-21 19:20:52 +0300

    chore(Main): add Main class to implement Bank App menu functionality

---
+++ src/gr/aueb/cf/ch18/bankapp/Main.java
@@ -0,0 +1,66 @@
+package gr.aueb.cf.ch18.bankapp;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Main {
+
+    private final static Scanner scanner = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+        String option;
+        String iban;
+        BigDecimal balance;                                 // BigDecimal αντί για double σε χρήματα λόγω ακρίβειας.
+
+
+        while (true) {
+            printMenu();
+            option = scanner.nextLine().trim();                   // Με τηω trim δεν παίρνουμε κενά στην αρχή ή στο τέλος!
+
+            try {
+                switch (option) {
+                    case "1" -> {
+                        System.out.println("Παρακαλώ εισάγεται το IBAN!");
+                        iban = scanner.nextLine().trim();
+                        System.out.println("Παρακαλώ εισάγεται το αρχικό υπόλοιπο!");
+                        balance = new BigDecimal(scanner.nextLine().trim());
+
+                        //AccountReadOnlyDTO readOnlyDTO = accountController.createNewAccount(iban, balance);
+                        //
+
+                    }
+
+
+
+
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+
+
+
+
+    }
+
+    private static void printMenu() {
+        System.out.println("\n=======================================");
+        System.out.println("|          Υπηρεσία Bank App            |");
+        System.out.println("========================================");
+        System.out.println("\nΥποσύστημα Τράπεζας");
+        System.out.println("    1. Δημιουργία / Ενημέρωση λογαριασμού");
+        System.out.println("    2. Προβολή λογαριασμών");
+        System.out.println("Υποσύστημα Πελατών");
+        System.out.println("    3. Κατάθεση");
+        System.out.println("    4. Ανάληψη");
+        System.out.println("    5. Ερώτηση υπολοίπου");
+        System.out.println("[Qq]. Έξοδος");
+        System.out.print("\n Εισάγετε μία επιλογή");
+    }
+
+
+
+}
