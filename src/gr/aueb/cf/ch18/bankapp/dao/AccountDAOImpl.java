c8faf30239b86597baf0de0370032ca1b48be275
sotikour <sotikour@gmail.com>
2026-09-23 19:20:55 +0300

    chore(DAO): update remove method in AccountDAOImpl to use removeIf for cleaner implementation

--- src/gr/aueb/cf/ch18/bankapp/dao/AccountDAOImpl.java
+++ src/gr/aueb/cf/ch18/bankapp/dao/AccountDAOImpl.java
@@ -27,7 +27,7 @@ public class AccountDAOImpl implements IAccountDAO{
 
     @Override
     public void remove(String iban) {
-
+        accounts.removeIf( account -> account.getIban().equals(iban));
     }
 
     @Override
